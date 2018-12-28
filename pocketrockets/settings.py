f79599cf5da297949d7346220248cbb23f172183
Simon Behar <simbeh7@gmail.com>
2018-12-27 19:41:50 -0800

    Updated settings (#1)

--- pocketrockets/settings.py
+++ pocketrockets/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from settings_secret import *
+from .settings_secret import _SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
