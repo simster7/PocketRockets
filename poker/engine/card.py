db0865c967fd8e04c90388a1ecc21a4862d8067f
Simon Behar Yutkewich <simon_beharyutkewich@intuit.com>
2019-01-03 08:41:40 -0800

    added initial implementation of card

---
+++ poker/engine/card.py
@@ -0,0 +1,51 @@
+
+SUIT_MAP = {
+        0: 'S',
+        1: 'H',
+        2: 'C',
+        3: 'D',
+    }
+
+RANK_MAP = {
+        0: '2',
+        1: '3',
+        2: '4',
+        3: '5',
+        4: '6',
+        5: '7',
+        6: '8',
+        7: '9',
+        8: 'T',
+        9: 'J',
+        10: 'Q',
+        11: 'K',
+        12: 'A',
+    }
+
+class Card:
+    def __init__(self, _id):
+        if _id < 0 or _id >= 52 or type(_id) != int:
+            raise Exception('Card id must be an integer in [0, 51]')
+        self.card_id = _id
+
+    @property
+    def suit_id(self):
+        return self.card_id // 13
+
+    @property
+    def suit(self):
+        return SUIT_MAP[self.suit_id]
+
+    @property
+    def rank_id(self):
+        return self.card_id % 13
+
+    @property
+    def rank(self):
+        return RANK_MAP[self.rank_id]
+
+    def __str__(self):
+        return '{}{}'.format(self.rank, self.suit)
+
+    def __repr__(self):
+        return 'Card: ' + str(self)
