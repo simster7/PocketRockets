63b4abdead88d5e58ac6cb862d79e7bc4f3b042b
Jarry Xiao <jarry.xiao@berkeley.edu>
2019-01-05 11:48:19 -0600

    renamed variables in card evaluator

--- poker/engine/evaluator.py
+++ poker/engine/evaluator.py
@@ -4,18 +4,18 @@ else:
     from .card import Card
 
 def straight_flush_check(hand):
-    suits = [card.s_id for card in hand]
-    frequencies = [s_id for s_id in set(suits) if suits.count(s_id) >= 5] 
+    suits = [card.suit for card in hand]
+    frequencies = [suit for suit in set(suits) if suits.count(suit) >= 5] 
     flush_hit = len(frequencies) == 1 
     if not flush_hit:
         return flush_hit, None
     suit = frequencies[0]
-    cards = [card for card in hand if card.s_id == suit]
+    cards = [card for card in hand if card.suit == suit]
     return straight_check(cards)
 
 def quad_check(hand):
     values = [card.rank for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) == 4]
+    frequencies = [rank_id for rank_id in set(values) if values.count(rank_id) == 4]
     hit = len(frequencies) == 1 
     if not hit:
         return False, None
@@ -23,11 +23,11 @@ def quad_check(hand):
 
 def boat_check(hand):
     values = [card.rank for card in hand]
-    trips = [v_id for v_id in set(values) if values.count(v_id) >= 3]
+    trips = [rank_id for rank_id in set(values) if values.count(rank_id) >= 3]
     if not trips:
         return False, None
     top_trip = max(trips)
-    pairs = [v_id for v_id in set(values) if values.count(v_id) >= 2 and v_id != top_trip]
+    pairs = [rank_id for rank_id in set(values) if values.count(rank_id) >= 2 and rank_id != top_trip]
     if not pairs:
         return False, None
     top_pair = max(pairs)
@@ -35,7 +35,7 @@ def boat_check(hand):
 
 def flush_check(hand):
     suits = [card.suit for card in hand]
-    frequencies = [s_id for s_id in set(suits) if suits.count(s_id) >= 5] 
+    frequencies = [suit for suit in set(suits) if suits.count(suit) >= 5] 
     hit = len(frequencies) == 1 
     if not hit:
         return hit, None
@@ -44,7 +44,7 @@ def flush_check(hand):
     return hit, (card.rank for card in order if card.suit == suit)[:5]
 
 def straight_check(hand):
-    values = sorted(list(set([card.v_id for card in hand])), key=lambda x: -x)
+    values = sorted(list(set([card.rank_id for card in hand])), key=lambda x: -x)
     if len(values) <  5:
         return False, None
     for i in range(len(values) - 4):
@@ -55,7 +55,7 @@ def straight_check(hand):
 
 def trip_check(hand):
     values = [card.rank for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) >= 3]
+    frequencies = [rank_id for rank_id in set(values) if values.count(rank_id) >= 3]
     hit = len(frequencies) >= 1
     trip = max(frequencies)
     remaining = sorted(list(set(values) - {trip}))
@@ -65,7 +65,7 @@ def trip_check(hand):
 
 def two_pair_check(hand):
     values = [card.rank for card in hand]
-    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 2], lambda x: -x)
+    frequencies = sorted([rank_id for rank_id in set(values) if values.count(rank_id) == 2], lambda x: -x)
     hit = len(frequencies) >= 2
     if not hit:
         return hit, None
@@ -81,7 +81,7 @@ def check_pair(hand):
     Returns remaining cards for tie-breaking, it is structured as ([pair rank id], *[sorted kicker rank ids])
     """
     values = [card.rank_id for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) == 2]
+    frequencies = [rank_id for rank_id in set(values) if values.count(rank_id) == 2]
     if not len(frequencies) == 1:
         return False, None
     pair = frequencies[0]
