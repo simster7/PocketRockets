62d14c2289a002c45f9ef9ee44a428c7dbe06fbd
Jarry Xiao <jarry.xiao@berkeley.edu>
2019-01-05 11:13:49 -0600

    added straight flush and straight

--- poker/holdem.py
+++ poker/holdem.py
@@ -46,8 +46,14 @@ class Game:
 # Here 'hand' should just be a list of cards
 
 def straight_flush_check(hand):
-    #TODO
-    return False, None
+    suits = [card.s_id for card in hand]
+    frequencies = [s_id for s_id in set(suits) if suits.count(s_id) >= 5] 
+    flush_hit = len(frequencies) == 1 
+    if not flush_hit:
+        return flush_hit, None
+    suit = frequencies[0]
+    cards = [card for card in hand if card.s_id == suit]
+    return straight_check(cards)
 
 def quad_check(hand):
     values = [card.v_id for card in hand]
@@ -80,13 +86,19 @@ def flush_check(hand):
     return hit, (card.v_id for card in order if card.s_id == suit)[:5]
 
 def straight_check(hand):
-    #TODO
+    values = sorted(list(set([card.v_id for card in hand])), key=lambda x: -x)
+    if len(values) <  5:
+        return False, None
+    for i in range(len(values) - 4):
+        constraints = all([values[j] - values[j+1] == 1 for j in range(i, i+4)])
+        if constraints:
+            return True, tuple(values[i:i+5])
     return False, None
 
 def trip_check(hand):
     values = [card.v_id for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) >= 3]
-    hit = len(frequencies) > 0
+    frequencies = [v_id for v_id in set(values) if values.count(v_id) == 3]
+    hit = len(frequencies) >= 1 
     if not hit:
         return hit, None
     trip = max(frequencies)
@@ -97,7 +109,7 @@ def trip_check(hand):
 
 def two_pair_check(hand):
     values = [card.v_id for card in hand]
-    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) >= 2], key=lambda x: -x)
+    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 2], key=lambda x: -x)
     hit = len(frequencies) >= 2
     if not hit:
         return hit, None
@@ -109,14 +121,14 @@ def two_pair_check(hand):
 
 def pair_check(hand):
     values = [card.v_id for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) >= 2]
-    hit = len(frequencies) >= 1 
+    frequencies = [v_id for v_id in set(values) if values.count(v_id) == 2]
+    hit = len(frequencies) == 1 
     if not hit:
         return hit, None
     pair = max(frequencies)
     remaining = sorted(list(set(values) - {pair}))
     kickers = [remaining.pop(), remaining.pop(), remaining.pop()]
-    return hit, (pair1, pair2, *kickers)
+    return hit, (pair, *kickers)
 
 def high_card_check(hand):
     values = tuple(sorted(list(set([card.v_id for card in hand])), key=lambda x: -x))
