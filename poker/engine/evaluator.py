6908bb8f790cc4fb0e73cae83bd4c18f70842de7
Simon Behar Yutkewich <simon_beharyutkewich@intuit.com>
2019-01-07 09:31:40 -0800

    progress with evaluator and tests

--- poker/engine/evaluator.py
+++ poker/engine/evaluator.py
@@ -43,41 +43,57 @@ def flush_check(hand):
     order = sorted(hand, lambda x: -x)
     return hit, (card.rank for card in order if card.suit == suit)[:5]
 
-def straight_check(hand):
-    values = sorted(list(set([card.v_id for card in hand])), key=lambda x: -x)
+def check_straight(hand):
+    """
+    Returns True if the hand conatins a straight, hand could be better than a straight and check_straight would still return true.
+    Returns high card for tie-breaking, it is structured as ([high card rank id],)
+    If hand is actually better than a three of a kind, this function returns undefined behavior.
+    """
+    values = sorted(list(set([card.rank_id for card in hand])), key=lambda x: -x)
     if len(values) <  5:
         return False, None
+    if 12 in values:
+        values.append(-1)
     for i in range(len(values) - 4):
-        constraints = all([values[j] - values[j+1] == 1 for j in range(i, i+4)])
+        constraints = all([(values[j] - values[j+1] == 1) for j in range(i, i+4)])
+        print(constraints, i, values)
         if constraints:
-            return True, tuple(values[i:i+5])
+            return True, (values[i],)
+
     return False, None
 
-def trip_check(hand):
-    values = [card.rank for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) >= 3]
-    hit = len(frequencies) >= 1
-    trip = max(frequencies)
-    remaining = sorted(list(set(values) - {trip}))
-    kicker1 = remaining.pop() 
-    kicker2 = remaining.pop()
-    return hit, (trip, kicker1, kicker2)
+def check_three_of_a_kind(hand):
+    """
+    Returns True if the hand conatins a three of a kind, hand could be better than a three of a kind and check_three_of_a_kind would still return true.
+    Returns remaining cards for tie-breaking, it is structured as ([three of a kind rank id], *[sorted kicker rank ids])
+    If hand is actually better than a three of a kind, this function returns undefined behavior.
+    """
+    values = [card.rank_id for card in hand]
+    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 3], key=lambda x: -x)
+    if not len(frequencies) >= 1:
+        return False, None
+    trip = frequencies[0]
+    remaining = sorted([value for value in values if value not in frequencies], key=lambda x: -x)[:2]
+    return True, (trip, *remaining)
 
-def two_pair_check(hand):
-    values = [card.rank for card in hand]
-    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 2], lambda x: -x)
-    hit = len(frequencies) >= 2
-    if not hit:
-        return hit, None
+def check_two_pair(hand):
+    """
+    Returns True if the hand conatins two pairs, hand could be better than two pair and check_two_pair would still return true.
+    Returns remaining cards for tie-breaking, it is structured as ([high pair rank id], [low pair rank id], *[sorted kicker rank ids])
+    If hand is actually better than two pair, this function returns undefined behavior.
+    """
+    values = [card.rank_id for card in hand]
+    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 2], key=lambda x: -x)
+    if not len(frequencies) >= 2:
+        return False, None
     pair1 = frequencies[0]
     pair2 = frequencies[1]
-    remaining = sorted(list(set(values) - {pair1, pair2}))
-    kicker = remaining.pop()
-    return hit, (pair1, pair2, kicker)
+    remaining = sorted([value for value in values if value not in [pair1, pair2]], key=lambda x: -x)[:1]
+    return True, (pair1, pair2, remaining[0])
 
 def check_pair(hand):
     """
-    Returns True if the hand conatins at least one pair, hand could be better than one pair and check_pair would still return true.
+    Returns True if the hand conatins at only one pair, hand could be better than one pair and check_pair would still return true.
     Returns remaining cards for tie-breaking, it is structured as ([pair rank id], *[sorted kicker rank ids])
     """
     values = [card.rank_id for card in hand]
--- poker/tests/evaluator_test.py
+++ poker/tests/evaluator_test.py
@@ -1,5 +1,6 @@
 from ..engine.util import hand_generator
 from ..engine.evaluator import *
+from ..engine.card import RANK_MAP
 
 
 class TestCheckHighCard:
@@ -52,3 +53,165 @@ class TestCheckPair:
         result = check_pair(pair_hand)
         assert result == (False, None)
 
+class TestCheckTwoPair:
+    """
+    check_two_pair
+    """
+
+    def test_two_pair_correct(self):
+        """
+        Two pair correctly evaluated
+        """
+        pair_hand = hand_generator('8S 8H TH TD 4C')
+        result = check_two_pair(pair_hand)
+        print(result)
+        assert result == (True, (8, 6, 2))
+
+    def test_two_pair_correct_with_seven(self):
+        """
+        Two pair correctly evaluated with seven cards
+        """
+        pair_hand = hand_generator('8S 8H TH TD 4C 6C 9S')
+        result = check_two_pair(pair_hand)
+        print(result)
+        assert result == (True, (8, 6, 7))
+
+    def test_two_pair_correct_with_seven_and_three_fair(self):
+        """
+        Two pair correctly evaluated with seven cards and precence of three pair
+        """
+        pair_hand = hand_generator('8S 8H TH TD 9C 9S 6C')
+        result = check_two_pair(pair_hand)
+        print(result)
+        assert result == (True, (8, 7, 6))
+
+    def test_no_pair_not_evaluated(self):
+        """
+        No two pair not evaluated, even with seven cards
+        """
+        pair_hand = hand_generator('8S 7H KH TD QC 2S 4H')
+        result = check_two_pair(pair_hand)
+        print(result)
+        assert result == (False, None)
+
+    def test_no_pair_not_evaluated_with_pair(self):
+        """
+        No two pair not evaluated, even with seven cards and a single pair
+        """
+        pair_hand = hand_generator('8S 8H KH TD QC 2S 4H')
+        result = check_two_pair(pair_hand)
+        print(result)
+        assert result == (False, None)
+
+class TestCheckThreeOfAKind:
+    """
+    check_three_of_a_kind
+    """
+
+    def test_three_of_a_kind_correct(self):
+        """
+        Three of a kind correctly evaluated
+        """
+        pair_hand = hand_generator('8S 8H 8C TD 4C')
+        result = check_three_of_a_kind(pair_hand)
+        print(result)
+        assert result == (True, (6, 8, 2))
+
+    def test_three_of_a_kind_correct_with_seven(self):
+        """
+        Three of a kind correctly evaluated, even with seven cards
+        """
+        pair_hand = hand_generator('8S 8H 8C TD 4C JC 2S')
+        result = check_three_of_a_kind(pair_hand)
+        print(result)
+        assert result == (True, (6, 9, 8))
+
+    def test_no_three_of_a_kind_evaluated_with_seven(self):
+        """
+        No three of a kind evaluated, even with seven cards
+        """
+        pair_hand = hand_generator('8S 8H 7C TD 4C JC 2S')
+        result = check_three_of_a_kind(pair_hand)
+        assert result == (False, None)
+
+    def test_three_of_a_kind_correct_with_seven_and_pair(self):
+        """
+        Three of a kind correctly evaluated, even with seven cards and extra pair (full house)
+        !! This is undefined behavior so if test fails in the future it is safe to modify/delete !!
+        """
+        pair_hand = hand_generator('8S 8H 8C TD TC JC 2S')
+        result = check_three_of_a_kind(pair_hand)
+        print(result)
+        assert result == (True, (6, 9, 8))
+
+    def test_three_of_a_kind_correct_with_seven_and_three(self):
+        """
+        Three of a kind correctly evaluated, even with seven cards and three of a kind (full house).
+        !! This is undefined behavior so if test fails in the future it is safe to modify/delete !!
+        """
+        pair_hand = hand_generator('8S 8H 8C TD TC TS 2S')
+        result = check_three_of_a_kind(pair_hand)
+        print(result)
+        assert result == (True, (8, 0))
+
+class TestCheckStraight:
+    """
+    check_straight
+    """
+
+    def test_check_straight(self):
+        """
+        Straight check works correctly
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D 6C')
+        result = check_straight(pair_hand)
+        assert result == (True, (4,))
+
+    def test_check_straight_with_seven(self):
+        """
+        Straight check works correctly, even with seven cards
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D 6C KC JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (4,))
+
+    def test_check_straight_with_seven_long(self):
+        """
+        Straight check works correctly, even with seven cards and straight is more than five cards
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D 6C 7C JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (5,))
+
+    def test_check_straight_with_seven_ace(self):
+        """
+        Straight check works correctly, even with seven cards and is ace low
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D AC 9C JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (3,))
+
+    def test_check_straight_with_seven_ace_high(self):
+        """
+        Straight check works correctly, even with seven cards and is ace high
+        """
+        pair_hand = hand_generator('TS QH KC 5D AC 9C JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (12,))
+
+    def test_check_straight_with_seven_no_straight(self):
+        """
+        No straight when there is not straight
+        """
+        pair_hand = hand_generator('8S QH KC 6D AC 9C JS')
+        result = check_straight(pair_hand)
+        assert result == (False, None)
+
+    def test_check_straight_with_seven_no_straight_wrap(self):
+        """
+        No straight when there is straight wrap-around
+        """
+        for start in [9, 10, 11]:
+            pair_hand = hand_generator(' '.join([RANK_MAP[card % 13] + 'S' for card in range(start, start + 5)]))
+            result = check_straight(pair_hand)
+            assert result == (False, None)
