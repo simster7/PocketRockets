acb50b98be939542b9691e798f46893fecdf11ea
Simon Behar <simbeh7@gmail.com>
2019-01-07 18:34:49 -0800

    Robust evaluator with full code-coverage (#3)
    * progress with evaluator and tests
    
    * Done with eval rework and tests
    
    * removed print statements
    
    * better test
    
    * made four of a kind return a tuple
    
    * no binary files
    
    * Jarrys comments: fixed flush detection, using reversed=True

--- poker/engine/evaluator.py
+++ poker/engine/evaluator.py
@@ -3,81 +3,108 @@ if __name__ == '__main__':
 else:
     from .card import Card
 
-def straight_flush_check(hand):
-    suits = [card.s_id for card in hand]
+def check_straight_flush(hand):
+    """
+    Returns True if the hand conatins a straight flush.
+    Returns composition of four of a kind for tie-breaking, it is structured as ([rank id])
+    """
+    suits = [card.suit_id for card in hand]
     frequencies = [s_id for s_id in set(suits) if suits.count(s_id) >= 5] 
-    flush_hit = len(frequencies) == 1 
-    if not flush_hit:
-        return flush_hit, None
+    if not len(frequencies) == 1:
+        return False, None
     suit = frequencies[0]
-    cards = [card for card in hand if card.s_id == suit]
-    return straight_check(cards)
+    cards = [card for card in hand if card.suit_id == suit]
+    return check_straight(cards)
 
-def quad_check(hand):
-    values = [card.rank for card in hand]
+def check_four_of_a_kind(hand):
+    """
+    Returns True if the hand conatins a four of a kind, hand could be better than a four of a kind and check_four_of_a_kind would still return true.
+    Returns composition of four of a kind for tie-breaking, it is structured as ([rank id])
+    """
+    values = [card.rank_id for card in hand]
     frequencies = [v_id for v_id in set(values) if values.count(v_id) == 4]
-    hit = len(frequencies) == 1 
-    if not hit:
+    if not len(frequencies) == 1:
         return False, None
-    return hit, (frequencies[0])
+    return True, (frequencies[0],)
 
-def boat_check(hand):
-    values = [card.rank for card in hand]
-    trips = [v_id for v_id in set(values) if values.count(v_id) >= 3]
-    if not trips:
-        return False, None
-    top_trip = max(trips)
-    pairs = [v_id for v_id in set(values) if values.count(v_id) >= 2 and v_id != top_trip]
-    if not pairs:
-        return False, None
-    top_pair = max(pairs)
-    return True, (top_trip, top_pair)
-
-def flush_check(hand):
-    suits = [card.suit for card in hand]
-    frequencies = [s_id for s_id in set(suits) if suits.count(s_id) >= 5] 
-    hit = len(frequencies) == 1 
-    if not hit:
-        return hit, None
-    suit = frequencies[0]
-    order = sorted(hand, lambda x: -x)
-    return hit, (card.rank for card in order if card.suit == suit)[:5]
-
-def straight_check(hand):
-    values = sorted(list(set([card.v_id for card in hand])), key=lambda x: -x)
-    if len(values) <  5:
-        return False, None
-    for i in range(len(values) - 4):
-        constraints = all([values[j] - values[j+1] == 1 for j in range(i, i+4)])
-        if constraints:
-            return True, tuple(values[i:i+5])
+def check_full_house(hand):
+    """
+    Returns True if the hand conatins a full house, hand could be better than a full house and check_full_house would still return true.
+    Returns composition of full house for tie-breaking, it is structured as ([trip rank id], [pair rank id])
+    """
+    trip_check = check_three_of_a_kind(hand)
+    pair_check = check_pair(hand)
+    if trip_check[0] and pair_check[0]:
+        return True, (trip_check[1][0], pair_check[1][0])
+    if trip_check[0]:
+        new_hand = [card for card in hand if card.rank_id != trip_check[1][0]]
+        second_trip_check = check_three_of_a_kind(new_hand)
+        if second_trip_check[0]:
+            return True, (trip_check[1][0], second_trip_check[1][0])
     return False, None
 
-def trip_check(hand):
-    values = [card.rank for card in hand]
-    frequencies = [v_id for v_id in set(values) if values.count(v_id) >= 3]
-    hit = len(frequencies) >= 1
-    trip = max(frequencies)
-    remaining = sorted(list(set(values) - {trip}))
-    kicker1 = remaining.pop() 
-    kicker2 = remaining.pop()
-    return hit, (trip, kicker1, kicker2)
+def check_flush(hand):
+    """
+    Returns True if the hand conatins a flush, hand could be better than a flush and check_flush would still return true.
+    Returns high card for tie-breaking, it is structured as ([high card rank id],)
+    """
+    suits = [card.suit_id for card in hand]
+    frequencies = [s_id for s_id in set(suits) if suits.count(s_id) >= 5] 
+    if not len(frequencies) == 1:
+        return False, None
+    suit = frequencies[0]
+    ordered_flush = sorted([card.rank_id for card in hand if card.suit_id == suit], reverse=True)[:5]
+    return True, tuple(ordered_flush)
 
-def two_pair_check(hand):
-    values = [card.rank for card in hand]
-    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 2], lambda x: -x)
-    hit = len(frequencies) >= 2
-    if not hit:
-        return hit, None
+def check_straight(hand):
+    """
+    Returns True if the hand conatins a straight, hand could be better than a straight and check_straight would still return true.
+    Returns high card for tie-breaking, it is structured as ([high card rank id],)
+    """
+    values = sorted(list(set([card.rank_id for card in hand])), reverse=True)
+    if len(values) <  5:
+        return False, None
+    if 12 in values:
+        values.append(-1)
+    for i in range(len(values) - 4):
+        constraints = all([(values[j] - values[j+1] == 1) for j in range(i, i+4)])
+        if constraints:
+            return True, (values[i],)
+
+    return False, None
+
+def check_three_of_a_kind(hand):
+    """
+    Returns True if the hand conatins a three of a kind, hand could be better than a three of a kind and check_three_of_a_kind would still return true.
+    Returns remaining cards for tie-breaking, it is structured as ([three of a kind rank id], *[sorted kicker rank ids])
+    If hand is actually better than a three of a kind, this function returns undefined behavior.
+    """
+    values = [card.rank_id for card in hand]
+    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 3], reverse=True)
+    if not len(frequencies) >= 1:
+        return False, None
+    trip = frequencies[0]
+    remaining = sorted([value for value in values if value not in frequencies], reverse=True)[:2]
+    return True, (trip, *remaining)
+
+def check_two_pair(hand):
+    """
+    Returns True if the hand conatins two pairs, hand could be better than two pair and check_two_pair would still return true.
+    Returns remaining cards for tie-breaking, it is structured as ([high pair rank id], [low pair rank id], *[sorted kicker rank ids])
+    If hand is actually better than two pair, this function returns undefined behavior.
+    """
+    values = [card.rank_id for card in hand]
+    frequencies = sorted([v_id for v_id in set(values) if values.count(v_id) == 2], reverse=True)
+    if not len(frequencies) >= 2:
+        return False, None
     pair1 = frequencies[0]
     pair2 = frequencies[1]
-    remaining = sorted(list(set(values) - {pair1, pair2}))
-    kicker = remaining.pop()
-    return hit, (pair1, pair2, kicker)
+    remaining = sorted([value for value in values if value not in [pair1, pair2]], reverse=True)[:1]
+    return True, (pair1, pair2, remaining[0])
 
 def check_pair(hand):
     """
-    Returns True if the hand conatins at least one pair, hand could be better than one pair and check_pair would still return true.
+    Returns True if the hand conatins at only one pair, hand could be better than one pair and check_pair would still return true.
     Returns remaining cards for tie-breaking, it is structured as ([pair rank id], *[sorted kicker rank ids])
     """
     values = [card.rank_id for card in hand]
@@ -85,14 +112,14 @@ def check_pair(hand):
     if not len(frequencies) == 1:
         return False, None
     pair = frequencies[0]
-    remaining = sorted([value for value in values if value != pair], key=lambda x: -x)[:3]
+    remaining = sorted([value for value in values if value != pair], reverse=True)[:3]
     return True, (pair, *remaining)
 
 def check_high_card(hand):
     """
     Always returns True, because hand is always at least high card good. Returns ordered cards for tie-breaking
     """
-    values = sorted([card.rank_id for card in hand], key=lambda x: -x)
+    values = sorted([card.rank_id for card in hand], reverse=True)
     return True, values[:5]
 
 
--- poker/tests/evaluator_test.py
+++ poker/tests/evaluator_test.py
@@ -1,5 +1,6 @@
 from ..engine.util import hand_generator
 from ..engine.evaluator import *
+from ..engine.card import RANK_MAP
 
 
 class TestCheckHighCard:
@@ -52,3 +53,304 @@ class TestCheckPair:
         result = check_pair(pair_hand)
         assert result == (False, None)
 
+class TestCheckTwoPair:
+    """
+    check_two_pair
+    """
+
+    def test_two_pair_correct(self):
+        """
+        Two pair correctly evaluated
+        """
+        pair_hand = hand_generator('8S 8H TH TD 4C')
+        result = check_two_pair(pair_hand)
+        assert result == (True, (8, 6, 2))
+
+    def test_two_pair_correct_with_seven(self):
+        """
+        Two pair correctly evaluated with seven cards
+        """
+        pair_hand = hand_generator('8S 8H TH TD 4C 6C 9S')
+        result = check_two_pair(pair_hand)
+        assert result == (True, (8, 6, 7))
+
+    def test_two_pair_correct_with_seven_and_three_fair(self):
+        """
+        Two pair correctly evaluated with seven cards and precence of three pair
+        """
+        pair_hand = hand_generator('8S 8H TH TD 9C 9S 6C')
+        result = check_two_pair(pair_hand)
+        assert result == (True, (8, 7, 6))
+
+    def test_no_pair_not_evaluated(self):
+        """
+        No two pair not evaluated, even with seven cards
+        """
+        pair_hand = hand_generator('8S 7H KH TD QC 2S 4H')
+        result = check_two_pair(pair_hand)
+        assert result == (False, None)
+
+    def test_no_pair_not_evaluated_with_pair(self):
+        """
+        No two pair not evaluated, even with seven cards and a single pair
+        """
+        pair_hand = hand_generator('8S 8H KH TD QC 2S 4H')
+        result = check_two_pair(pair_hand)
+        assert result == (False, None)
+
+class TestCheckThreeOfAKind:
+    """
+    check_three_of_a_kind
+    """
+
+    def test_three_of_a_kind_correct(self):
+        """
+        Three of a kind correctly evaluated
+        """
+        pair_hand = hand_generator('8S 8H 8C TD 4C')
+        result = check_three_of_a_kind(pair_hand)
+        assert result == (True, (6, 8, 2))
+
+    def test_three_of_a_kind_correct_with_seven(self):
+        """
+        Three of a kind correctly evaluated, even with seven cards
+        """
+        pair_hand = hand_generator('8S 8H 8C TD 4C JC 2S')
+        result = check_three_of_a_kind(pair_hand)
+        assert result == (True, (6, 9, 8))
+
+    def test_no_three_of_a_kind_evaluated_with_seven(self):
+        """
+        No three of a kind evaluated, even with seven cards
+        """
+        pair_hand = hand_generator('8S 8H 7C TD 4C JC 2S')
+        result = check_three_of_a_kind(pair_hand)
+        assert result == (False, None)
+
+    def test_three_of_a_kind_correct_with_seven_and_pair(self):
+        """
+        Three of a kind correctly evaluated, even with seven cards and extra pair (full house)
+        !! This is undefined behavior so if test fails in the future it is safe to modify/delete !!
+        """
+        pair_hand = hand_generator('8S 8H 8C TD TC JC 2S')
+        result = check_three_of_a_kind(pair_hand)
+        assert result == (True, (6, 9, 8))
+
+    def test_three_of_a_kind_correct_with_seven_and_three(self):
+        """
+        Three of a kind correctly evaluated, even with seven cards and three of a kind (full house).
+        !! This is undefined behavior so if test fails in the future it is safe to modify/delete !!
+        """
+        pair_hand = hand_generator('8S 8H 8C TD TC TS 2S')
+        result = check_three_of_a_kind(pair_hand)
+        assert result == (True, (8, 0))
+
+class TestCheckStraight:
+    """
+    check_straight
+    """
+
+    def test_check_straight(self):
+        """
+        Straight check works correctly
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D 6C')
+        result = check_straight(pair_hand)
+        assert result == (True, (4,))
+
+    def test_check_straight_with_seven(self):
+        """
+        Straight check works correctly, even with seven cards
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D 6C KC JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (4,))
+
+    def test_check_straight_with_seven_long(self):
+        """
+        Straight check works correctly, even with seven cards and straight is more than five cards
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D 6C 7C JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (5,))
+
+    def test_check_straight_with_seven_ace(self):
+        """
+        Straight check works correctly, even with seven cards and is ace low
+        """
+        pair_hand = hand_generator('2S 3H 4C 5D AC 9C JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (3,))
+
+    def test_check_straight_with_seven_ace_high(self):
+        """
+        Straight check works correctly, even with seven cards and is ace high
+        """
+        pair_hand = hand_generator('TS QH KC 5D AC 9C JS')
+        result = check_straight(pair_hand)
+        assert result == (True, (12,))
+
+    def test_check_straight_with_seven_no_straight(self):
+        """
+        No straight when there is not straight
+        """
+        pair_hand = hand_generator('8S QH KC 6D AC 9C JS')
+        result = check_straight(pair_hand)
+        assert result == (False, None)
+
+    def test_check_straight_with_seven_no_straight_wrap(self):
+        """
+        No straight when there is straight wrap-around
+        """
+        for start in [9, 10, 11]:
+            pair_hand = hand_generator(' '.join([RANK_MAP[card % 13] + 'S' for card in range(start, start + 5)]))
+            result = check_straight(pair_hand)
+            assert result == (False, None)
+
+class TestCheckFlush:
+    """
+    check_flush
+    """
+
+    def test_check_flush(self):
+        """
+        Flush check works correctly
+        """
+        pair_hand = hand_generator('JS 3S TS 5S 6S')
+        result = check_flush(pair_hand)
+        assert result == (True, (9, 8, 4, 3, 1))
+
+    def test_check_flush_with_seven(self):
+        """
+        Flush check works correctly with seven cards, gets high card correctly
+        """
+        pair_hand = hand_generator('JS 3S TS 5S 6S AS 8C')
+        result = check_flush(pair_hand)
+        assert result == (True, (12, 9, 8, 4, 3))
+
+    def test_check_flush_with_no_flush(self):
+        """
+        Flush not detected when there is no flush
+        """
+        pair_hand = hand_generator('JS 3S TS 5S 6C AC 8C')
+        result = check_flush(pair_hand)
+        assert result == (False, None)
+
+class TestCheckFullHouse:
+    """
+    check_full_house
+    """
+
+    def test_check_full_house(self):
+        """
+        Full house check works correctly
+        """
+        pair_hand = hand_generator('JS JC JD 6C 6S')
+        result = check_full_house(pair_hand)
+        assert result == (True, (9, 4))
+
+    def test_check_full_house_seven(self):
+        """
+        Full house check works correctly with seven cards
+        """
+        pair_hand = hand_generator('AS AC AD 6C 6S 2S KC')
+        result = check_full_house(pair_hand)
+        assert result == (True, (12, 4))
+
+    def test_check_full_house_seven_two_trip(self):
+        """
+        Full house check works correctly with seven cards and two three of a kinds
+        """
+        pair_hand = hand_generator('JS JC JD 6C 6S 6D KC')
+        result = check_full_house(pair_hand)
+        assert result == (True, (9, 4))
+
+    def test_check_full_house_seven_two_trip_order_agnostic(self):
+        """
+        Full house check works correctly with seven cards and two three of a kinds, order agnostic
+        """
+        pair_hand = hand_generator('6C 6S 6D JS JC JD KC')
+        result = check_full_house(pair_hand)
+        assert result == (True, (9, 4))
+
+    def test_check_full_house_seven_two_pair(self):
+        """
+        No full house when there is no full house
+        """
+        pair_hand = hand_generator('JS JC KD 6C 6S 5D KC')
+        result = check_full_house(pair_hand)
+        assert result == (False, None)
+
+class TestCheckFourOfAKind:
+    """
+    check_four_of_a_kind
+    """
+
+    def test_check_four_of_a_kind(self):
+        """
+        Four of a kind check works correctly
+        """
+        pair_hand = hand_generator('JS JC JD JH AS')
+        result = check_four_of_a_kind(pair_hand)
+        assert result == (True, (9,))
+
+    def test_check_four_of_a_kind_with_seven(self):
+        """
+        Four of a kind check works correctly with seven cards
+        """
+        pair_hand = hand_generator('2S 2C 2D 2H AS KC 7C')
+        result = check_four_of_a_kind(pair_hand)
+        assert result == (True, (0,))
+
+    def test_check_four_of_a_kind_with_seven_and_trips(self):
+        """
+        Four of a kind check works correctly with seven cards and extra trips
+        """
+        pair_hand = hand_generator('5S 5C 5D 5H AS AC AH')
+        result = check_four_of_a_kind(pair_hand)
+        assert result == (True, (3,))
+
+    def test_check_four_of_a_kind_with_seven_no_four(self):
+        """
+        No four of a kind when there is none
+        """
+        pair_hand = hand_generator('JS JC JD 6C 6S 6D KC')
+        result = check_four_of_a_kind(pair_hand)
+        assert result == (False, None)
+
+class TestStraightFlush:
+    """
+    check_straight_flush
+    """
+
+    def test_check_straight_flush(self):
+        """
+        Straight flush check works correctly
+        """
+        pair_hand = hand_generator('4S 5S 6S 7S 8S')
+        result = check_straight_flush(pair_hand)
+        assert result == (True, (6,))
+
+    def test_check_straight_flush_seven(self):
+        """
+        Straight flush check works correctly even with seven cards
+        """
+        pair_hand = hand_generator('7S 8S 9S TS JS AH TC')
+        result = check_straight_flush(pair_hand)
+        assert result == (True, (9,))
+
+    def test_check_straight_flush_seven_no_flush(self):
+        """
+        No straight flush if there is no flush
+        """
+        pair_hand = hand_generator('7S 8S 9H TS JS AH TC')
+        result = check_straight_flush(pair_hand)
+        assert result == (False, None)
+
+    def test_check_straight_flush_seven_no_straight(self):
+        """
+        No straight flush if there is no straight
+        """
+        pair_hand = hand_generator('6S 8S 9S TS JS AH TC')
+        result = check_straight_flush(pair_hand)
+        assert result == (False, None)
