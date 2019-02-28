a82608d720af2c3e18f3c506dacf6d8f87a84791
Simon Behar <simbeh7@gmail.com>
2019-02-28 13:31:13 -0800

    Frontend and engine refactors (#15)
    * Dockerfile changes
    
    * Added react
    
    * New refactor
    
    * progress with refactor

--- backend/poker/engine/player.py
+++ backend/poker/engine/player.py
@@ -1,15 +1,23 @@
 from typing import Optional
 
+from .action import Action
+
 
 class Player:
     name: str
     stack: int
     seat_number: Optional[int]
+    folded: bool
+    last_action: Optional[Action]
+    sitting_out: bool
 
     def __init__(self, _name: str) -> None:
         self.name = _name
         self.stack = 0
         self.seat_number = None
+        self.folded = False
+        self.last_action = None
+        self.sitting_out = False
 
     def make_bet(self, bet_size: int) -> int:
         assert self.stack >= bet_size, "Player may not bet amount larger than their stack"
@@ -19,24 +27,6 @@ class Player:
     def receive_pot(self, pot_size: int) -> int:
         self.stack += pot_size
         return pot_size
-
-    def set_stack(self, stack: int) -> None:
-        self.stack = stack
-
-    def get_stack(self) -> int:
-        return self.stack
-
-    def set_name(self, name: str) -> None:
-        self.name = name
-
-    def get_name(self) -> str:
-        return self.name
-
-    def set_seat_number(self, seat_number: Optional[int]) -> None:
-        self.seat_number = seat_number
-
-    def get_seat_number(self) -> int:
-        return self.seat_number
 
     def __repr__(self):
         return str(self.__dict__)
