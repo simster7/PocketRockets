25220c643fc075db4cf0f616bf1e3348fc3df1e3
Simon Behar <simbeh7@gmail.com>
2019-03-09 18:14:33 -0800

    formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,17 @@ import './App.css';
 import PokerClient from './components/PokerClient';
 
 class App extends React.Component {
-  
-  public render() {
-    return (
-      <div className="App">
-        {/* <header className="App-header">
+	public render() {
+		return (
+			<div className="App">
+				{/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header> */}
-        <PokerClient roomName="simon" />
-      </div>
-    );
-  }
+				<PokerClient roomName="simon" />
+			</div>
+		);
+	}
 }
 
 export default App;
