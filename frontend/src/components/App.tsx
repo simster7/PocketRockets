a9c5529b70a1ff36914072ad5905e1f5a161d5d1
Simon Behar <simbeh7@gmail.com>
2019-08-18 14:45:52 -0700

    New frontend skeleton code

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (<div>
+                <h1>Welcome to React with Typescript</h1>
+                <p>The color of this page is: red</p>
+            </div>
+        );
+    }
+}
+
+export default App;
