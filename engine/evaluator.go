4143e0ac7ab151608b904c5ba0a501d77a232e02
Simon Behar <simbeh7@gmail.com>
2020-06-15 09:12:07 -0700

    Fix bug in Straight evaluator

--- engine/evaluator.go
+++ engine/evaluator.go
@@ -158,6 +158,7 @@ func CheckStraight(hand []Card) (bool, Tiebreakers) {
 	}
 	handRanks := getCardRankIdSlice(hand)
 	descendingSort(handRanks)
+	handRanks = deduplicateInts(handRanks)
 	// If the highest hand rank is 12 (i.e. an ace) add a -1 to allow for
 	// low end straights
 	if handRanks[0] == 12 {
@@ -295,6 +296,27 @@ func descendingSort(vs []int) {
 		// Use a "greater than" function instead of "less than" to sort in descending order
 		return vs[i] > vs[j]
 	})
+}
+
+// in must be sorted in descending order
+func deduplicateInts(in []int) []int {
+	if len(in) == 0 {
+		return in
+	}
+
+	currMax := in[0]
+	dedup := []int{in[0]}
+	for i := 0; i < len(in); i++ {
+		if in[i] > currMax {
+			panic("input to deduplicateInts must be sorted in decreasing order")
+		}
+		if in[i] == dedup[len(dedup)-1] {
+			continue
+		}
+		currMax = in[i]
+		dedup = append(dedup, in[i])
+	}
+	return dedup
 }
 
 func filterInt(vs []int, f func(int) bool) []int {
